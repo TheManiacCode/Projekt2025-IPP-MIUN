970a77e7805b55095768a1268dd83f27d8a603cd
Johan <lycky1213@gmail.com>
2025-04-28 11:42:23 +0200

    Skräpa ultraljud sensor. display output only
    Pga komplikationer med många kablar och pins som inte blivit lödda tillsammans skriver jag kod för att göra att displayen endast visar nedräkning när man klickar på knappen för att representera när ljudsensorn aktiveras.

--- BaseIPP/BaseIPP.ino
+++ BaseIPP/BaseIPP.ino
@@ -1,18 +1,37 @@
-#include <NewPing.h>
-NewPing sonar(3, 2, 800); // Trig=3, Echo=2, Max distance=800cm
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+#define BUTTON_PIN A0 // Knapparna är anslutna till analog pin A0
 
 void setup() {
-  Serial.begin(9600);
+    lcd.begin(16, 2);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Tryck Start");
 }
 
 void loop() {
-  int distance = sonar.ping_cm();
-  delay(500);
+    int buttonValue = analogRead(BUTTON_PIN);
 
-  if (distance <= 400)
-  {
-    Serial.print("Distance= ");
-    Serial.print(distance);
-    Serial.println(" cm");
-  }
+    if (buttonValue < 100) { // Justering beroende på vilken knapp som används
+        startTimer();
+    }
+}
+
+void startTimer() {
+    for (int i = 30; i >= 0; i--) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Timer: ");
+        lcd.print(i);
+
+        lcd.setCursor(0, 1);
+        int progress = map(i, 0, 30, 0, 16);
+        for (int j = 0; j < progress; j++) {
+            lcd.print("#");
+        }
+
+        delay(1000);
+    }
 }
