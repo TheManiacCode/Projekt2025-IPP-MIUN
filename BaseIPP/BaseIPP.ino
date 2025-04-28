7eb6b3d247ba34e0fe4ec17627a82ef0c9715002
Johan <lycky1213@gmail.com>
2025-04-28 13:31:01 +0200

    Bug fixar
    Har fixat saknade tecken då åäö inte stöds på denna skärm.
    Skriver ut att man ska klicka på den vänstra för att börja som start prompt.
    Har ej kunnat få plats med både skärm och ultraljud sensor på samma brödbord ännu.

--- BaseIPP/BaseIPP.ino
+++ BaseIPP/BaseIPP.ino
@@ -2,37 +2,53 @@
 #include <Olimex16x2.h>
 
 Olimex16x2 lcd;
-
+bool finishTimer = false;
 void setup() {
     lcd.begin();
     lcd.setBacklight(255);
     lcd.clear();
-    lcd.drawLine(0, "Tryck Start");
-}
+    lcd.drawLine(0, "tryck vanstra");
+    lcd.drawLine(1, "for att starta");
+    }
 
 void loop() {
     if (lcd.readButton(0)) 
     {
         startTimer();
     }
+    if (finishTimer)
+    {
+        lcd.clear();
+        lcd.drawLine(0, "Bra skrubbat");
+        delay(500);
+        finishTimer = false;
+        lcd.clearLine(0);
+        lcd.clearLine(1);
+        lcd.drawLine(0, "tryck vanstra");
+        lcd.drawLine(1, "for att starta");
+    }
 }
 
 void startTimer() {
-    int i;
+    short i;
     for (i = 30; i >= 0; i--) {
         lcd.clear();
         lcd.drawLine(0, " Timer: " + String(i)); 
-    int tmp = i/4;
+    short tmp = i/3;
         String progressBar = " ";
-        for (int j = 0; j < tmp; j++) 
+        for (short j = 0; j < tmp; j++) 
         {
             progressBar += "#";
         }
+        if (i > 0 && tmp <= 0)
+        {
+            progressBar = "#";
+        }
         lcd.drawLine(1, progressBar);
-
         delay(800);
         lcd.clearLine(0);
         lcd.clearLine(1);
         delay(200);
     }
+    finishTimer = true;
 }
